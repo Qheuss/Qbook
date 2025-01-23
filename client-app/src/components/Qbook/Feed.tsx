b0897c1803c2461505b025fa8768e26e30c1bf25
Qheuss <36819862+Qheuss@users.noreply.github.com>
2025-01-23 22:21:26 +0100

    celui-ci par

--- client-app/src/components/Qbook/Feed.tsx
+++ client-app/src/components/Qbook/Feed.tsx
@@ -8,7 +8,7 @@ const Feed = () => {
       <Title />
       <Carousel />
       <Post
-        text="Ayant récemment terminé la formation Junior Développeur chez BeCode et complété un stage enrichissant chez EVS Broadcast, je suis à la recherche d'une opportunité pour mettre à profit mes compétences dans un environnement professionnel. Éligible au dispositif PFI, je suis ponctuel, dévoué, à l’aise dans les environnements collaboratifs et impatient de contribuer à une équipe tout en continuant à développer mes compétences."
+        text="Ayant récemment terminé la formation Junior Développeur chez BeCode et complété celui-ci par un stage enrichissant chez EVS Broadcast, je suis à la recherche d'une opportunité pour mettre à profit mes compétences dans un environnement professionnel. Éligible au dispositif PFI, je suis ponctuel, dévoué, à l’aise dans les environnements collaboratifs et impatient de contribuer à une équipe tout en continuant à développer mes compétences."
         images={['images/photocv.jpg']}
       />
       <Post
