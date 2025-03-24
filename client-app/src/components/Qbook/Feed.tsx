2f420a6af453beab1b9a0bb19594d994928c264a
Qheuss <36819862+Qheuss@users.noreply.github.com>
2025-03-24 18:31:06 +0100

    Update Feed.tsx

--- client-app/src/components/Qbook/Feed.tsx
+++ client-app/src/components/Qbook/Feed.tsx
@@ -14,20 +14,7 @@ const Feed = () => {
     >
       <Title />
       <Carousel />
-      <Post>
-        <p className={theme === 'dark' ? 'text-fontDark' : 'text-fontLight'}>
-          Ayant récemment terminé la formation Junior Développeur chez BeCode et
-          complété celui-ci par un stage enrichissant chez EVS Broadcast, je
-          suis à la recherche d'une opportunité pour mettre à profit mes
-          compétences dans un environnement professionnel. Éligible au
-          dispositif PFI, je suis ponctuel, dévoué, à l’aise dans les
-          environnements collaboratifs et impatient de contribuer à une équipe
-          tout en continuant à développer mes compétences.
-        </p>
-        <section>
-          <img src='images/QuentinHeusse.jpg' alt='Quentin Heusse' />
-        </section>
-      </Post>
+
       <Post>
         <p className={theme === 'dark' ? 'text-fontDark' : 'text-fontLight'}>
           Réalisations:
@@ -53,6 +40,22 @@ const Feed = () => {
           </li>
         </ul>
       </Post>
+
+      <Post>
+        <p className={theme === 'dark' ? 'text-fontDark' : 'text-fontLight'}>
+          Ayant récemment terminé la formation Junior Développeur chez BeCode et
+          complété celui-ci par un stage enrichissant chez EVS Broadcast, je
+          suis à la recherche d&apos;une opportunité pour mettre à profit mes
+          compétences dans un environnement professionnel. Éligible au
+          dispositif PFI, je suis ponctuel, dévoué, à l&apos;aise dans les
+          environnements collaboratifs et impatient de contribuer à une équipe
+          tout en continuant à développer mes compétences.
+        </p>
+        <section>
+          <img src='images/QuentinHeusse.jpg' alt='Quentin Heusse' />
+        </section>
+      </Post>
+
       <Post>
         <p className={theme === 'dark' ? 'text-fontDark' : 'text-fontLight'}>
           COMPÉTENCES TECH: React.js | Next.js | HTML | Figma | Linux | C# |
