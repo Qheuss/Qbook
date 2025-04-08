43b7e153edbd45867ae86c31018658b850a4cd36
Qheuss <36819862+Qheuss@users.noreply.github.com>
2025-04-08 13:38:23 +0200

    @ pathname for imports from /src

--- client-app/vite.config.ts
+++ client-app/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import viteReact from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +11,9 @@ export default defineConfig({
     tailwindcss(),
     viteReact(),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
