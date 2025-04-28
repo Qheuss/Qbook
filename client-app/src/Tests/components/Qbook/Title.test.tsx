5d956ce3a5c36c0b353c33a52a6917cd11c338eb
Qheuss <36819862+Qheuss@users.noreply.github.com>
2025-04-28 15:50:36 +0200

    Update Title.test.tsx

--- client-app/src/Tests/components/Qbook/Title.test.tsx
+++ client-app/src/Tests/components/Qbook/Title.test.tsx
@@ -36,46 +36,37 @@ describe('Title', () => {
     expect(buttonLinkedin).toBeInTheDocument();
     expect(buttonGithub).toBeInTheDocument();
 
-    // Click on the buttons
     await userEvent.click(buttonLinkedin);
     await userEvent.click(buttonGithub);
 
-    // Check that openUrl URLs
     expect(openUrl).toHaveBeenCalledWith(
       'https://www.linkedin.com/in/quentin-heusse'
     );
     expect(openUrl).toHaveBeenCalledWith('https://github.com/Qheuss');
 
-    // Check the number of times openUrl was called
     expect(openUrl).toHaveBeenCalledTimes(2);
   });
 
   test('Check if the dialog shows up', async () => {
     renderComponent();
 
-    // Get the button and click on it
     const buttonPortfolio = screen.getByText(/Cliquez ici pour me contacter!/i);
     expect(buttonPortfolio).toBeInTheDocument();
     await userEvent.click(buttonPortfolio);
 
-    // Check if the dialog appeared
     const dialog = screen.getByRole('dialog');
     expect(dialog).toBeInTheDocument();
 
-    // Check if the dialog disappears when the escape key is pressed
     await userEvent.keyboard('{Escape}');
     expect(dialog).not.toBeInTheDocument();
 
-    // Check if the dialog appears again when the button is clicked again
     await userEvent.click(buttonPortfolio);
     const dialog2 = screen.getByRole('dialog');
     expect(dialog2).toBeInTheDocument();
 
-    // Check if the dialog disappears when the enter key is pressed
     await userEvent.keyboard('{Enter}');
     expect(dialog2).not.toBeInTheDocument();
 
-    // Check if navigation happens when clicking the button again (e.g., after dialog close)
     expect(mockNavigate).toHaveBeenCalledWith('/contact');
   });
 });
